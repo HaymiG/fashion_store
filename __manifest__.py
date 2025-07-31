177b92221be19c9f062f98417a5b3a19c5191cb5
haymi_g <haymig75@gmail.com>
2025-07-31 19:48:34 +0300

    updated manifest file

--- __manifest__.py
+++ __manifest__.py
@@ -8,12 +8,16 @@
     'data': [
         'security/ir.model.access.csv',
         'data/sequence.xml',
-        # 'views/menu_views.xml',
+        #'views/menu_views.xml',
         'views/website_templates.xml',
         'views/cloth_item_views.xml',
-         'views/cloth_product_views.xml',
-          'views/website_footer.xml',
-        #   'views/website_header.xml',
+        'views/cloth_product_views.xml',
+        'views/website_footer.xml',
+        'views/cloth_shop_order_views.xml',
+        'views/custom_contact_page.xml',
+        #'views/website_header.xml',
+        'report/report_templates.xml',
+        'report/report_actions.xml',
         
        
     ],
@@ -23,9 +27,10 @@
     'assets': {
         'web.assets_frontend': [
             'shop/static/src/css/custom_footer.css',
-            # 'shop/static/src/css/custom_header.css',  
+            'shop/static/src/css/custom_header.css',  
             'shop/static/src/css/homepage.css',
             'shop/static/src/img/homepage.png',
+            # 'shop/static/src/css/contact.css',
           
         ],
     },
