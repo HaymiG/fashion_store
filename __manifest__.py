d840125a8c2cb5fe83ee001dac189eacc27d7256
haymi_g <haymig75@gmail.com>
2025-08-11 10:03:41 +0300

    manifest file updated

--- __manifest__.py
+++ __manifest__.py
@@ -4,18 +4,18 @@
     'summary': 'Custom clothing store with POS and website integration',
     'category': 'Sales',
     'author': 'Your Name',
-    'depends': ['base', 'website', 'product', 'point_of_sale'],
+    'depends': ['base', 'website','website_sale', 'product', 'point_of_sale'],
     'data': [
         'security/ir.model.access.csv',
         'data/sequence.xml',
-        #'views/menu_views.xml',
         'views/website_templates.xml',
         'views/cloth_item_views.xml',
         'views/cloth_product_views.xml',
         'views/website_footer.xml',
-        'views/cloth_shop_order_views.xml',
+        'views/order_views.xml',
         'views/custom_contact_page.xml',
-        #'views/website_header.xml',
+        'views/custom_header.xml',
+       
         'report/report_templates.xml',
         'report/report_actions.xml',
         
@@ -29,8 +29,9 @@
             'shop/static/src/css/custom_footer.css',
             'shop/static/src/css/custom_header.css',  
             'shop/static/src/css/homepage.css',
-            'shop/static/src/img/homepage.png',
             # 'shop/static/src/css/contact.css',
+            'shop/static/src/img/homepage.png',
+            'shop/static/src/img/logo.png',
           
         ],
     },
