8cf4d994cb36c8154e2a3b4980d81c0406543a00
haymi_g <haymig75@gmail.com>
2025-10-27 11:31:32 +0300

    cloth items

--- models/__init__.py
+++ models/__init__.py
@@ -1,3 +1,5 @@
 from . import cloth_item
 from . import cloth_product
 from . import order
+from . import product_category
+from . import contact_message
--- models/cloth_item.py
+++ models/cloth_item.py
@@ -1,4 +1,5 @@
-from odoo import models, fields
+from odoo import models, fields, api
+
 
 class ClothShopItem(models.Model):
     _name = "cloth.shop.item"
@@ -17,3 +18,20 @@ class ClothShopItem(models.Model):
         ('xl', 'XL'),
     ], string='Size')
     category_id = fields.Many2one('cloth.shop.category', string='Cloth Category')
+    _sql_constraints = [
+    ('unique_product_id', 'unique(product_id)', 'Each product can only appear once in the shop!')
+]
+    
+    @api.model
+    def create(self, vals):
+        record = super().create(vals)
+        if record.product_id and not record.product_id.website_published:
+            record.product_id.sudo().write({'website_published': True})
+        return record
+
+    def write(self, vals):
+        res = super().write(vals)
+        for rec in self:
+            if rec.product_id and not rec.product_id.website_published:
+                rec.product_id.sudo().write({'website_published': True})
+        return res
